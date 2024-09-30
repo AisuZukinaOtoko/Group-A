dceccf1e45070135fb0b82631c3e7b726a98fd00
DARILETS <lldaphrich@gmail.com>
2024-09-30 19:32:18 +0200

    unexpose api key

--- src/BuildingMap.jsx
+++ src/BuildingMap.jsx
@@ -30,7 +30,7 @@ const BuildingMap = () => {
 
   useEffect(() => {
     const loader = new Loader({
-      apiKey: "AIzaSyA8Xe4zjl1vMpWtm3JCIE_G7Mz7zhHSD2Y",
+      apiKey: "",
       version: "weekly",
       libraries: ["places"],
     });
